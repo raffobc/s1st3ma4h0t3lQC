57d831512915b8c5b5e89ce29e6924c5a935adb7
raffobc <raffo.bustamante@gmail.com>
2026-03-16 15:46:32 -0500

    security: proteger credenciales DB via GitHub Secrets en deploy

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 define('BASE_PATH', dirname(__DIR__));
 define('BASE_URL', rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '')), '/'));
-define('MASTER_DB_HOST', 'localhost');
-define('MASTER_DB_NAME', 'hotel_master');
-define('MASTER_DB_USER', 'root');
-define('MASTER_DB_PASS', '');
+define('MASTER_DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('MASTER_DB_NAME', getenv('DB_NAME') ?: 'hotel_master');
+define('MASTER_DB_USER', getenv('DB_USER') ?: 'root');
+define('MASTER_DB_PASS', getenv('DB_PASS') ?: '');
 date_default_timezone_set('America/Lima');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+$isProd = (getenv('APP_ENV') === 'production');
+error_reporting($isProd ? 0 : E_ALL);
+ini_set('display_errors', $isProd ? '0' : '1');
 ?>
