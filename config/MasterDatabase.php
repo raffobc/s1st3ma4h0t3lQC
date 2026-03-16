b118dbb82efbeebb4d935a930360b4ecaa87e055
raffobc <raffo.bustamante@gmail.com>
2026-03-16 16:17:06 -0500

    fix: usar credenciales de config en MasterDatabase

--- config/MasterDatabase.php
+++ config/MasterDatabase.php
@@ -5,9 +5,9 @@ class MasterDatabase {
     public static function getConnection(): PDO {
         if (self::$instance === null) {
             self::$instance = new PDO(
-                "mysql:host=localhost;dbname=hotel_master;charset=utf8mb4",
-                'root',
-                '',
+                "mysql:host=" . MASTER_DB_HOST . ";dbname=" . MASTER_DB_NAME . ";charset=utf8mb4",
+                MASTER_DB_USER,
+                MASTER_DB_PASS,
                 [
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
